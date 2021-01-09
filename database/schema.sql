bd186587a0fe1852354693130bf15faf5739e6e2
dhaura <dhaurapathirana@gmail.com>
2021-01-10 00:10:38 +0530

    test 2

--- database/schema.sql
+++ database/schema.sql
@@ -106,4 +106,4 @@ ALTER TABLE "session"
     ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 
 CREATE INDEX "IDX_session_expire" ON "session" ("expire");
----------------
+--------------------------------------------------------------------
