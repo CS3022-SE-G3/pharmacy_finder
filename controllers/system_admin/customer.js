ec398ca911029a4fc7b65a2e8e384fc8d2aa8e04
Mahesh Madushan <madu140@gmail.com>
2021-01-13 00:29:29 +0530

    view customer information by system admin (API)

--- controllers/system_admin/customer.js
+++ controllers/system_admin/customer.js
@@ -1,5 +1,118 @@
-
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
+const { pool } = require('../../database/connection');
 
-module.exports = router;
+/**
+ * 
+ * @param {number} accountId - customerId
+ */
+function validateAccountId(accountId){
+
+    // schema to validate
+    const schema = Joi.object({
+        
+        "accountId"    : Joi.number().integer().required(),
+        
+    });
+
+    // return valid or not
+    return schema.validate(accountId)
+}
+
+/**
+ * 
+ * @param {number} accountId - customerId
+ */
+async function getCustomerAccountInfromation(accountId){
+
+    try {
+        const response = await new Promise((resolve, reject) => {
+            // if query succces we gonna resolve the result
+            // else we gonna reject it
+
+            const qry = "SELECT full_name,nic,email,address,gender,dob,contact_no FROM Customer WHERE customer_id=?"; // query
+
+            // send th qry
+            pool.query(qry,[accountId], (err, res) =>{
+                if (err){
+                    reject (new Error(err.message));
+                } 
+                // else
+                console.log(res)
+                resolve(res);
+            })
+        }
+        )
+
+        return response;
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+/**
+ * 
+ * @param {request} req - request to API
+ * @param {response} res - response
+ *
+ *  handles response and return customer infromation
+ */
+const view_customer_information = (req, res) => {
+
+    
+    // get accountId from URL
+    const accountId = req.params.accountId; 
+
+    // validating
+    const {error} = validateAccountId({accountId:accountId});
+
+    if (error) {
+
+        // log the error
+        console.error('ValidationError:system_admin-customer_account_id: '+error.details[0].message)
+
+        // send bad request
+        res.status(400).json({
+            description:'Invalid account Id',
+            error:true,
+        })
+
+        res.end()
+
+        // stop execution
+        return
+    }
+
+    // get the account information of the customer as requested
+    const result = getCustomerAccountInfromation(accountId);
+
+    result.then((data) => {
+
+        if(data.length === 0){
+            res.status(400).json({
+                description:'Account Id not found',
+                error:true
+            })
+
+            return
+        }
+        
+        // send data to front end
+        res.status(200).json(data[0])
+        res.end()
+    })
+    .catch(error => {
+        console.log(error)
+
+        // send 'internal server error'
+        res.status(200).json({
+            description:'Internal server error',
+            error:true
+        })
+    })
+
+}
+
+module.exports = view_customer_information;
--- routes/system_admin/customer.js
+++ routes/system_admin/customer.js
@@ -1,5 +1,14 @@
 // view customer info
 const express = require('express');
 const router = express.Router();
+const view_customer_information = require('../../controllers/system_admin/customer');
 
+/**
+ * @view_customer_information  - module that handle system admin's request to view customer information
+ * @route - localhost:3000/system_admin/customer/viewcustomerinformation/{account id of the customer}
+ * 
+ * route to view customer information by system admin
+ */
+
+router.get('/viewcustomerinformation/:accountId',view_customer_information)
 module.exports = router;
