ae8e068efa0c8a968ae89248a3c6aacfcd9d2308
dhaura <dhaurapathirana@gmail.com>
2021-01-13 23:58:48 +0530

    pharmacy signup view page

---
+++ public/assets/register.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(){
+  
+        // var item = $('form input');
+        // var todo = {item: item.val()};
+  
+        $.ajax({
+          type: 'POST',
+          url: '/',
+          // data: todo,
+          success: function(data){
+            location.reload();
+          }
+        });
+  
+        return false;
+  
+    });
+  });
+  
--- routes/pharmacy/signup.js
+++ routes/pharmacy/signup.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const { signupPharmacy } = require('../../controllers/pharmacy/signup');
+const bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+// URL: localhost3000/pharmacy/signup, method is GET
+router.get('/', (request, response) => {
+    response.render('pharmacy/signup');
+});
+
+// URL: localhost3000/customer/signup, method is POST
+// router.post('/', signupPharmacy);
 
 module.exports = router;
