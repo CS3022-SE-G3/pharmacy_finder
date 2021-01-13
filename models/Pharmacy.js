a9da9f3d8f97e6088a399e3dd4ba9d1e9fef4abd
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-01-14 02:28:33 +0530

    getPendingPharmacies in model

--- models/Pharmacy.js
+++ models/Pharmacy.js
@@ -3,18 +3,32 @@ const { pool } = require('../database/connection');
 class Pharmacy{
 
     static getPharmacyInfo(pharmacyId) {
-    return new Promise((resolve, reject) =>{
-        const result = pool.query('SELECT pharmacy_id,name,address,email,contact_no,approved_state FROM pharmacy WHERE pharmacy_id = ?',
-            [pharmacyId],
-            function (error, results, fields) {
-                if (error) {
-                    reject (new Error(error.message));
+        return new Promise((resolve, reject) =>{
+            const result = pool.query('SELECT pharmacy_id,name,address,email,contact_no,approved_state FROM pharmacy WHERE pharmacy_id = ?',
+                [pharmacyId],
+                function (error, results, fields) {
+                    if (error) {
+                        reject (new Error(error.message));
+                    }
+                    resolve(results);
                 }
-                resolve(results);
-            }
-        )
-    } )
-}
+            )
+        } )
+    }
+
+    static getPendingPharmacies(){
+        return new Promise((resolve, reject) =>{
+            const result = pool.query('SELECT pharmacy_id,name,address,email,contact_no FROM pharmacy WHERE approved_state = ?',
+                ['Not Approved'],
+                function (error, results, fields) {
+                    if (error) {
+                        reject (new Error(error.message));
+                    }
+                    resolve(results);
+                }
+            )
+        } )
+    }
 
 }
 
