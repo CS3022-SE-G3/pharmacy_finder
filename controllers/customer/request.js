4517efe9345434107e8854f86c11024f645f466c
CharithaRathnayake <61895303+CharithaRathnayake@users.noreply.github.com>
2021-01-15 00:58:10 +0530

    Charitha/view-broadcasted-requests

--- controllers/customer/request.js
+++ controllers/customer/request.js
@@ -1,5 +1,72 @@
 
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
+const { pool } = require('../../database/connection');
+const customer = require('../../models/Customer');
 
-module.exports = router;
+/**
+ * 
+ * @param {number} customerId
+ */
+function validateCustomerId(customerId){
+
+    // schema to validate
+    const schema = Joi.object({
+        
+        "customerId"    : Joi.number().integer().min(10001).required(),
+        
+    });
+
+    // return valid or not
+    return schema.validate(customerId)
+}
+
+
+const view_broadcasted_requests = (req, res) => {
+
+    // get customerId from URL
+    const customerId = req.params.id; 
+
+    // validating
+    const {error} = validateCustomerId({customerId:customerId});
+
+    if (error) {
+
+        // log the error
+        console.error('ValidationError:customer-customerId: '+error.details[0].message)
+
+        // send bad request
+        res.status(400).send("Invalid Customer");
+
+        res.end()
+
+        // stop execution
+        return
+    }
+
+    // get the information of the broadcasted requests as requested
+    const result = customer.getBroadcastedRequests(customerId);
+
+    result.then((data) => {
+
+        if(data.length === 0){
+            return res.status(400).send(" customerID not found");
+            
+        }
+        
+        // send data to front end
+        
+        res.status(200).json(data)
+        res.end()
+    })
+    .catch(error => {
+        console.log(error)
+
+        // send 'internal server error'
+        res.status(500).send("Internal Server Error")
+    })
+
+}
+
+module.exports = view_broadcasted_requests;
--- routes/customer/request.js
+++ routes/customer/request.js
@@ -3,5 +3,8 @@
 // update broadcasted request
 const express = require('express');
 const router = express.Router();
+const view_broadcasted_requests=require('../../controllers/customer/request');
 
+//URL:localhost:3000/customer/request/view/:id --method GET
+router.get('/view/:id',view_broadcasted_requests);
 module.exports = router;
