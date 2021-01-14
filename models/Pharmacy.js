80bced472ba09a0032bdcf57b187aa24c7d2ebd1
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-01-14 21:32:37 +0530

    remove duplicate method getPharmacyInfo

--- models/Pharmacy.js
+++ models/Pharmacy.js
@@ -48,21 +48,8 @@ class Pharmacy{
                         console.log(result.sql);
                         reject(error);
                         return;
-                    };
-                    resolve(console.log("Done"));
-                }
-            )
-        })
-    }
-    static getPharmacyInfo(pharmacyId) {
-        return new Promise((resolve, reject) => {
-            const result = pool.query('SELECT pharmacy_id,name,address,email,contact_no,approved_state FROM pharmacy WHERE pharmacy_id = ?',
-                [pharmacyId],
-                function (error, results, fields) {
-                    if (error) {
-                        reject(new Error(error.message));
                     }
-                    resolve(results);
+                    resolve(console.log("Done"));
                 }
             )
         })
