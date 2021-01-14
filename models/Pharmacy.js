dbd5796a1e4831f881878cbbe97a68125dbaaa5d
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-01-14 17:40:16 +0530

    added longitude,latitude to Pharmacy queries

--- models/Pharmacy.js
+++ models/Pharmacy.js
@@ -4,7 +4,7 @@ class Pharmacy{
 
     static getPharmacyInfo(pharmacyId) {
         return new Promise((resolve, reject) =>{
-            const result = pool.query('SELECT pharmacy_id,name,address,email,contact_no,approved_state FROM pharmacy WHERE pharmacy_id = ?',
+            const result = pool.query('SELECT pharmacy_id,name,address,longitude,latitude,email,contact_no,approved_state FROM pharmacy WHERE pharmacy_id = ?',
                 [pharmacyId],
                 function (error, results, fields) {
                     if (error) {
@@ -18,7 +18,7 @@ class Pharmacy{
 
     static getPendingPharmacies(){
         return new Promise((resolve, reject) =>{
-            const result = pool.query('SELECT pharmacy_id,name,address,email,contact_no FROM pharmacy WHERE approved_state = ?',
+            const result = pool.query('SELECT pharmacy_id,name,address,longitude,latitude,email,contact_no,approved_state FROM pharmacy WHERE approved_state = ?',
                 ['Not Approved'],
                 function (error, results, fields) {
                     if (error) {
