42423b72e1c713120ab534f732509d8b669123f0
Nagitha Abeywickrema <62171036+NagithaAbeywickrema@users.noreply.github.com>
2021-01-14 20:26:12 +0530

    MOD - system_admin/drug endpoint

--- models/SystemAdmin.js
+++ models/SystemAdmin.js
@@ -1,6 +1,116 @@
 const { pool } = require('../database/connection');
 
+
 class SystemAdmin{
+
+    /**
+     * @description Add a branded drug to the database
+     */
+    static enterDrug(drug) {
+        return new Promise((resolve, reject) => {
+            const query = pool.query("INSERT INTO branded_drug(`brand_name`,`manufacturer`,`drug_type_id`) VALUES (?,?,?)",
+                [
+                    drug.brand_name,
+                    drug.manufacturer,
+                    drug.drug_type_id
+                ],
+                function (error, results, fields) {
+                    if (error) {
+                        console.log(query.sql);
+                        reject(error);
+                        return;
+                    };
+                    resolve(console.log("Done"));
+                }
+            )
+        })
+    }
+
+    /**
+     * @description Get all branded drugs from the database
+     */
+    static getAllDrugs() { 
+        return new Promise((resolve, reject) => {
+            const query = pool.query("SELECT branded_drug_id,brand_name,manufacturer,drug_type_id FROM branded_drug WHERE is_deleted = ?",
+                [false],
+                function (error, results, fields) {
+                    if (error) {
+                        console.log(query.sql);
+                        reject(error);
+                        return;
+                    };
+                    console.log(results);
+                    resolve(results[0]);
+                }
+            )
+        })
+    }
+
+    /**
+     * @description Get all drug types from database
+     * @todo Add 'is_deleted' to drug_type table
+     */
+    static getAllDrugTypes() {
+        return new Promise((resolve, reject) => {
+            const query = pool.query("SELECT drug_type_name FROM drug_type",
+                function (error, results, fields) {
+                    if (error) {
+                        console.log(query.sql);
+                        reject(error);
+                        return;
+                    };
+                    console.log(results);
+                    resolve(results[0]);
+                }
+            )
+        })
+    }
+
+    /**
+     * @description Update a branded drug's details in the database
+     */
+    static updateDrug(drug) {
+        return new Promise((resolve, reject) => {
+            const query = pool.query("UPDATE branded_drug SET brand_name = ?, manufacturer = ?, drug_type_id = ? WHERE branded_drug_id = ?",
+                [
+                    drug.brand_name,
+                    drug.manufacturer,
+                    drug.drug_type_id,
+                    drug.branded_drug_id
+                ],
+                function (error, results, fields) {
+                    if (error) {
+                        console.log(query.sql);
+                        reject(error);
+                        return;
+                    };
+                    resolve(console.log("Done"));
+                }
+            )
+        })
+    }
+
+    /**
+     * @description Delete a branded drug from the database
+     */
+    static deleteDrug(drug) {
+        return new Promise((resolve, reject) => {
+            const query = pool.query("UPDATE branded_drug SET is_deleted = ? WHERE branded_drug_id = ?",
+                [
+                    true,
+                    drug.branded_drug_id
+                ],
+                function (error, results, fields) {
+                    if (error) {
+                        console.log(query.sql);
+                        reject(error);
+                        return;
+                    };
+                    resolve(console.log("Done"));
+                }
+            )
+        })
+    }
 
 }
 
--- routes/system_admin/drug.js
+++ routes/system_admin/drug.js
@@ -1,154 +1,68 @@
-// add new drug
-// remove drug
-// update drug info
-// view all drugs
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const _ = require('lodash');
+
 const {
-    getAllDrugs,
-    validateAddDrugDetails,
-    validateUpdateDrugDetails,
-    enterDrug,
-    updateDrug,
+    addNewDrug,
+    viewAllDrugs,
+    viewAddDrugForm,
+    viewUpdateDrugForm,
+    updateDrugDetails,
+    viewDeleteDrugPrompt,
     deleteDrug
 } = require('../../controllers/system_admin/drug');
-const {
-    getAllDrugTypes
-} = require('../../controllers/system_admin/drug_type');
 
-// URL: localhost3000/system_admin/drug, method is GET 
-/**@todo return the results in response body */
-router.get('/', async (request, response) => {
-    try
-    {
-        const result = await getAllDrugs();
-        console.log(result);
-        return response.status(200).send(result);
-    }
-    catch (error) {
-        return response.status(500).send("internal server error");
-    }
-    //return response.sendFile(path.join(__dirname, '../../views/system_admin/drug.html')); 
-});
+/**
+ * @description Load and view all branded drugs
+ * @URL localhost:3000/system_admin/drug
+ * @method GET
+ * @todo return results in response body along with the html file
+ */
+router.get('/', viewAllDrugs);
 
-// URL: localhost3000/system_admin/drug/create, method is GET 
-/**@todo return the results in response body */
-router.get('/create', async (request, response) => {
-    try
-    {
-        const result = await getAllDrugTypes();
-        console.log(result);
-        return response.status(200).send(result);
-    }
-    catch (error) {
-        console.log(error);
-        return response.status(500).send("internal server error");
-    }
-    //return response.sendFile(path.join(__dirname, '../../views/system_admin/add_new_drug_form.html')); 
-});
+/**
+ * @description Promt form to add a branded drug
+ * @URL localhost:3000/system_admin/drug/add
+ * @method GET
+ * @todo return results in response body along with the html file
+ */
+router.get('/add', viewAddDrugForm);
 
-// URL: localhost3000/system_admin/drug/create, method is POST
-router.post('/create', async (request, response) => {
+/**
+ * @description Add a new branded drug
+ * @URL localhost3000/system_admin/drug/add
+ * @method POST
+ */
+router.post('/add', addNewDrug);
 
-    const { error } = validateAddDrugDetails(_.pick(request.body,
-        [
-            "brand_name",
-            "manufacturer",
-            "drug_type_id"
-        ]
-    ));
+/**
+ * @description Promt form to update a branded drug's details
+ * @URL localhost:3000/system_admin/drug/update
+ * @method GET
+ * @todo return results in response body along with the html file
+ */
+router.get('/update', viewUpdateDrugForm);
 
-    if (error) {
-        return response.status(400).send(error.message);
-    }
+/**
+ * @description Update existing branded drug's details
+ * @URL localhost:3000/system_admin/drug
+ * @method PUT
+ */
+router.put('/update', updateDrugDetails);
 
-    try {
-        const result = await enterDrug(_.pick(request.body,
-            [
-                "brand_name",
-                "manufacturer",
-                "drug_type_id"
-            ]
-        ));
-    return response.status(200).send(request.body);
+/**
+ * @description Prompt delete branded drug option
+ * @URL localhost:3000/system_admin/drug/delete
+ * @method GET
+ * @todo return results in response body along with the html file
+ */
+router.get('/delete', viewDeleteDrugPrompt);
 
-
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send("Internal Server Error");
-    }
-});
-
-// URL: localhost3000/system_admin/drug/update, method is GET 
-/**@todo return the results in response body */
-router.get('/update', (request, response) => {
-    try
-    {
-        const results = getAllDrugTypes();
-        return response.status(200).send(results);
-
-    }
-    catch (error) {
-        return response.status(500).send("Internal Server Error");
-    }
-    //return response.sendFile(path.join(__dirname, '../../views/system_admin/add_new_drug_form.html')); 
-});
-
-// URL: localhost3000/system_admin/drug/update, method is POST
-router.put('/update', async (request, response) => {
-
-    const { error } = validateUpdateDrugDetails(_.pick(request.body,
-        [
-            "branded_drug_id",
-            "brand_name",
-            "manufacturer",
-            "drug_type_id"
-        ]
-    ));
-
-    if (error) {
-        return response.status(400).send(error.message);
-    }
-
-    try {
-        const result = await updateDrug(_.pick(request.body,
-            [
-                "branded_drug_id",
-                "brand_name",
-                "manufacturer",
-                "drug_type_id"
-            ]
-        ));
-
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send("Internal Server Error");
-    }
-    return response.status(200).send(request.body);
-});
-
-// URL: localhost3000/system_admin/drug/delete, method is GET 
-/**@todo return the results in response body */
-router.get('/delete', (request, response) => {
-    //return response.sendFile(path.join(__dirname, '../../views/system_admin/add_new_drug_form.html')); 
-});
-
-// URL: localhost3000/system_admin/drug/delete, method is POST
-router.put('/delete', async (request, response) => {
-
-    try {
-        const result = await deleteDrug(_.pick(request.body,
-            [
-                "branded_drug_id"
-            ]
-        ));
-    return response.status(200).send(request.body);
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send("Internal Server Error");
-    }
-});
+/**
+ * @description Delete specicfied branded drug
+ * @URL localhost:3000/system_admin/drug/delete
+ * @method PUT
+ * @todo return results in response body along with the html file
+ */
+router.put('/delete', deleteDrug);
 
 module.exports = router;
