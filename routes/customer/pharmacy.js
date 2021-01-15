73c7b763599e2a82bd18302669095059559ea0a2
CharithaRathnayake <61895303+CharithaRathnayake@users.noreply.github.com>
2021-01-15 15:40:38 +0530

    Added a description for viewPharmacyInformation

--- routes/customer/pharmacy.js
+++ routes/customer/pharmacy.js
@@ -4,7 +4,12 @@ const express = require('express');
 const router = express.Router();
 const {viewPharmacyInformation} = require('../../controllers/customer/pharmacy');
 
-//URL:localhost:3000/customer/pharmacy/view/:name --method GET
+/**
+ * @description Load and view requested pharmacy information
+ * @URL localhost:3000/system_admin/drug
+ * @method GET
+ * @todo return results in response body along with the html file
+ */
 router.get('/view/:name', viewPharmacyInformation);
 
 module.exports = router;
