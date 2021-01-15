9f88f2c99ce69a2a5d43a1b57f2512c8cc22e883
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-15 09:37:33 +0530

    minor fixes

--- routes/customer/request.js
+++ routes/customer/request.js
@@ -3,8 +3,10 @@
 // update broadcasted request
 const express = require('express');
 const router = express.Router();
-const view_broadcasted_requests=require('../../controllers/customer/request');
+const {viewBroadcastedRequests} = require('../../controllers/customer/request');
 
 //URL:localhost:3000/customer/request/view/:id --method GET
-router.get('/view/:id',view_broadcasted_requests);
+router.get('/view/:id', viewBroadcastedRequests);
+
+
 module.exports = router;
