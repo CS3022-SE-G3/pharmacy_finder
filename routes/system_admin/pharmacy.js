5d2a5cd75d97927dca689afa1680afc7ce818148
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2021-01-15 09:07:31 +0530

    added method doc blocks in routes

--- routes/system_admin/pharmacy.js
+++ routes/system_admin/pharmacy.js
@@ -6,13 +6,28 @@ const router = express.Router();
 
 const {viewPharmacyInfo,viewPendingPharmacies,approvePharmacy} = require('../../controllers/system_admin/pharmacy');
 
-//GET http://localhost:3000/system_admin/pharmacy/pending
+/**
+ * @description Load the data of all approval pending pharmacies
+ * @URL http://localhost:3000/system_admin/pharmacy/pending
+ * @method GET
+ * @todo return results in response body in html file
+ */
 router.get('/pending',viewPendingPharmacies);
 
-//GET http://localhost:3000/system_admin/pharmacy/view/{pharmacyid}
+/**
+ * @description Load the data of a pharmacy given a pharmacy ID
+ * @URL http://localhost:3000/system_admin/pharmacy/view/{pharmacyid}
+ * @method GET
+ * @todo return results in response body in html file
+ */
 router.get('/view/:pharmacyid', viewPharmacyInfo);
 
-//PUT http://localhost:3000/system_admin/pharmacy/approve/{pharmacyid}
+/**
+ * @description Approve a pharmacy that is not already approved
+ * @URL http://localhost:3000/system_admin/pharmacy/approve/{pharmacyid}
+ * @method PUT
+ * @todo notify the pharmacy of approval
+ */
 router.put('/approve/:pharmacyid',approvePharmacy);
 
 module.exports = router;
