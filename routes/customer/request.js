239d5537f4a01167d1f4a65619a3c21fae00debd
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-15 15:12:05 +0530

    get broadcast form route added

--- routes/customer/request.js
+++ routes/customer/request.js
@@ -3,10 +3,11 @@
 // update broadcasted request
 const express = require('express');
 const router = express.Router();
-const {viewBroadcastedRequests} = require('../../controllers/customer/request');
+const { viewBroadcastedRequests } = require('../../controllers/customer/request');
+const { getBroadcastForm } = require('../../controllers/customer/request');
 
 //URL:localhost:3000/customer/request/view/:id --method GET
 router.get('/view/:id', viewBroadcastedRequests);
 
-
+router.get('/broadcast', getBroadcastForm);
 module.exports = router;
