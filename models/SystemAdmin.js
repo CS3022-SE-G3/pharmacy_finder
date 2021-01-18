bd8665da88bc23351513629674388028260fa750
Mahesh Madushan <madu140@gmail.com>
2021-01-18 07:25:33 +0530

    refactored

--- models/SystemAdmin.js
+++ models/SystemAdmin.js
@@ -209,7 +209,39 @@ class SystemAdmin{
         console.log(error)
     }
     }
-/**
+
+
+    /**
+ *  
+ *  @description - getting customer infromation from database from accountID
+ *  @param {number} accountId - customerId
+ */
+    static async getCustomerAccountInformation(accountId){
+
+    try {
+        const response = await new Promise((resolve, reject) => {
+            // if query succces we gonna resolve the result
+            // else we gonna reject it
+            const qry = "SELECT full_name,nic,email,address,gender,dob,contact_no FROM customer WHERE customer_id=?"; // query
+            pool.query(qry,[accountId], (err, res) =>{
+                if (err){
+                    reject (new Error(err.message));
+                } 
+                // else
+                console.log(res)
+                resolve(res);
+            })
+        }
+        )
+
+        return response;
+
+    } catch (error) {
+        console.log(error)
+    }
+    }
+
+ /**
  *  
  *  @description - getting customer infromation from database from accountID
  *  
@@ -243,36 +275,6 @@ class SystemAdmin{
             }
         }
 
-        /**
- *  
- *  @description - getting customer infromation from database from accountID
- *  @param {number} accountId - customerId
- */
-    static async getCustomerAccountInformation(accountId){
-
-    try {
-        const response = await new Promise((resolve, reject) => {
-            // if query succces we gonna resolve the result
-            // else we gonna reject it
-            const qry = "SELECT full_name,nic,email,address,gender,dob,contact_no FROM customer WHERE customer_id=?"; // query
-
-            pool.query(qry,[accountId], (err, res) =>{
-                if (err){
-                    reject (new Error(err.message));
-                } 
-                // else
-                console.log(res)
-                resolve(res);
-            })
-        }
-        )
-
-        return response;
-
-    } catch (error) {
-        console.log(error)
-    }
-    }
 
         /**
  *  
