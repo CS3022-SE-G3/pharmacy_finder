6b8133a00be5d0ea9be39752d93c5402403e7295
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-21 14:47:43 +0530

    updates

--- routes/customer/request.js
+++ routes/customer/request.js
@@ -17,12 +17,15 @@ router.get('/view', viewAllRequests);
 
 
 /**
- * @description Load and view a specific request's details of a customer
+ * @description Load and view a specific request details of a customer
  * @URL localhost:3000/customer/request/view/:requestId 
  * @method GET
  * @todo return results in response body along with the html file
  */
 router.get('/view/req/:requestId', viewBroadcastedRequests);
+
+
+
 
 //URL localhost:3000/customer/request/broadcast --method GET
 router.get('/broadcast', getBroadcastForm);
