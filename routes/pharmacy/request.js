e1ce38eb250d8c26a451619e9b227c9996fa47ec
Gayangi Seneviratne <sgayangi@gmail.com>
2021-01-21 15:44:36 +0530

    updates

--- routes/pharmacy/request.js
+++ routes/pharmacy/request.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const router = express.Router();
 const { viewAllRequests } = require('../../controllers/pharmacy/request');
+const isAPharmacy = require('../../middleware/isAPharmacy');
 
 /**
  * @URL localhost:3000/pharmacy/request/view
  * view all requests for a pharmacy
  */
-router.get('/view',viewAllRequests);
+router.get('/view',isAPharmacy, viewAllRequests);
 
 module.exports = router;
