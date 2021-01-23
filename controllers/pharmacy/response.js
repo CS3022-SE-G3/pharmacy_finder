d92cfe9695d9ae851a916ac78934ca7a6046cb3e
dhaura <dhaurapathirana@gmail.com>
2021-01-23 17:32:56 +0530

    pasindu request reponse updates

--- controllers/pharmacy/response.js
+++ controllers/pharmacy/response.js
@@ -1,12 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Pharmacy = require('../../models/Pharmacy');
+const path = require('path');
+const { request } = require('express');
+
 async function displayRequestInfo(info, res, request_id) {
     try {
-        const userInfo = await getCustomerInfo(request_id);
+        const userInfo = await Pharmacy.getCustomerInfo(request_id);
         info.push(userInfo);
-        const requestedDrugTypes = await getRequestedDrugTypes(request_id);
+        const requestedDrugTypes = await Pharmacy.getRequestedDrugTypes(request_id);
         info.push(requestedDrugTypes);
-        const requestedBrandedDrugs = await getRequestedBrandedDrugs(request_id);
+        const requestedBrandedDrugs = await Pharmacy.getRequestedBrandedDrugs(request_id);
         info.push(requestedBrandedDrugs);
-        res.render("pharmacy/request_info", {
+        return res.render("pharmacy/request_info", {
             info: info
         });
     } catch (e) {
@@ -14,33 +20,35 @@ async function displayRequestInfo(info, res, request_id) {
     }
 }
 
-async function storeResponseInfo(res, drug_type_ids, branded_drug_ids, response_id, request_id, pharmacy_id) {
+async function storeResponseInfo(res, drug_type_ids, branded_drug_ids, pharmacy_id, request_id) {
     try {
-        const sIR = await storeInResponse(response_id, request_id, pharmacy_id);
+        const response = await Pharmacy.getResponseID(pharmacy_id, request_id);
+        const sIR = await Pharmacy.storeInResponse(response[0].response_id, request_id, pharmacy_id);
         console.log(sIR);
-        const sADT = await storeAcceptedDrugTypes(response_id, drug_type_ids);
+        const sADT = await Pharmacy.storeAcceptedDrugTypes(response[0].response_id, drug_type_ids);
         console.log(sADT);
-        const sABD = await storeAcceptedBrandedDrugs(response_id, branded_drug_ids);
+        const sABD = await Pharmacy.storeAcceptedBrandedDrugs(response[0].response_id, branded_drug_ids);
         console.log(sABD);
-        res.status(200).send("Successfully Stored the Response...");
+        return res.status(200).send("Successfully Stored the Response...");
     } catch (e) {
         console.log(e.message);
     }
 }
 
-async function displayResponseInfo(info, res, response_id, request_id) {
+async function displayResponseInfo(info, res, pharmacy_id, request_id) {
     try {
-        const userInfo = await getCustomerInfo(request_id);
+        const userInfo = await Pharmacy.getCustomerInfo(request_id);
         info.push(userInfo);
-        const requestedDrugTypes = await getRequestedDrugTypes(request_id);
+        const requestedDrugTypes = await Pharmacy.getRequestedDrugTypes(request_id);
         info.push(requestedDrugTypes);
-        const requestedBrandedDrugs = await getRequestedBrandedDrugs(request_id);
+        const requestedBrandedDrugs = await Pharmacy.getRequestedBrandedDrugs(request_id);
         info.push(requestedBrandedDrugs);
-        const respondedDrugTypes = await getRespondedDrugTypes(response_id);
+        const response = await Pharmacy.getResponseID(pharmacy_id, request_id);
+        const respondedDrugTypes = await Pharmacy.getRespondedDrugTypes(response[0].responses_id);
         info.push(respondedDrugTypes);
-        const respondedBrandedDrugs = await getRespondedBrandedDrugs(response_id);
+        const respondedBrandedDrugs = await Pharmacy.getRespondedBrandedDrugs(response[0].response_id);
         info.push(respondedBrandedDrugs);
-        res.render("pharmacy/edit_response", {
+        return res.render("pharmacy/edit_response", {
             info: info
         });
     } catch (e) {
@@ -50,17 +58,18 @@ async function displayResponseInfo(info, res, response_id, request_id) {
 
 
 
-async function storeEditedResponseInfo(res, drug_type_ids, branded_drug_ids, response_id, request_id, pharmacy_id) {
+async function storeEditedResponseInfo(res, drug_type_ids, branded_drug_ids, pharmacy_id, request_id) {
     try {
-        const dPR = await deletePreviousRespone(response_id);
+        const response = await Pharmacy.getResponseID(pharmacy_id, request_id);
+        const dPR = await Pharmacy.deletePreviousRespone(response[0].response_id);
         console.log(dPR);
-        const sIR = await storeInResponse(response_id, request_id, pharmacy_id);
+        const sIR = await Pharmacy.storeInResponse(response[0].response_id, request_id, pharmacy_id);
         console.log(sIR);
-        const sADT = await storeAcceptedDrugTypes(response_id, drug_type_ids);
+        const sADT = await Pharmacy.storeAcceptedDrugTypes(response[0].response_id, drug_type_ids);
         console.log(sADT);
-        const sABD = await storeAcceptedBrandedDrugs(response_id, branded_drug_ids);
+        const sABD = await Pharmacy.storeAcceptedBrandedDrugs(response[0].response_id, branded_drug_ids);
         console.log(sABD);
-        res.status(200).send("Successfully Stored the Edited Response...");
+        return res.status(200).send("Successfully Stored the Edited Response...");
     } catch (e) {
         console.log(e.message);
     }
