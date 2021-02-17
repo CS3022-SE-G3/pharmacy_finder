be4ddb188cc189d17f7b46a31f36ac2325621c56
Gayangi Seneviratne <sgayangi@gmail.com>
2021-02-18 02:08:06 +0530

    customer tests updated

---
+++ tests/integration/customer/pharmacy/viewPharmacyInformation.test.js
@@ -0,0 +1,62 @@
+const { viewPharmacyInformation } = require('../../../../controllers/customer/pharmacy');
+let server;
+
+describe('viewPharmacyInformation', () => {
+    const req = {
+        params: {
+            "pharmacy_name": "Helix Pharmacy"
+        }
+    };
+
+    const res = {
+        render: jest.fn()
+    };
+
+    beforeEach(() => {
+        server = require('../../../../index');
+    });
+
+    afterEach( () => {
+        server.close();
+    });
+
+    it("200 OK route", async () => {
+        const expectedResult = {
+            pharmacyInformation: {
+                pharmacy_id: 30001,
+                name: 'Helix Pharmacy',
+                address: '433/2, Galedanda, Gonawala.',
+                email: 'info@helixpharm.com',
+                contact_no: 112911280
+            },
+            pageTitle: 'Pharmacy Information'
+        }
+        await viewPharmacyInformation(req, res);
+        expect(res.render).toHaveBeenCalledWith("customer/view_pharmacy", expectedResult);
+    });
+
+
+    it("400 - Invalid pharmacy name provided", async () => {
+        req.params.pharmacy_name = "as";
+        const expectedResult = {
+            err_data: "Invalid pharmacy name provided",
+            redirect_to: "/customer/pharmacy",
+            button_message: "Try Again",
+            form_method: "GET"
+        }
+        await viewPharmacyInformation(req, res);
+        expect(res.render).toHaveBeenCalledWith("400", expectedResult);
+    });
+
+    it("404 - Pharmacy not found", async () => {
+        req.params.pharmacy_name = new Array(10).join("asd"); //autogenerates a string
+        await viewPharmacyInformation(req, res);
+        expect(res.render).toHaveBeenCalledWith("404");
+    });
+
+    it("500 - Internal server error", async () => {
+        /**
+         * @todo internal server error testing
+         */
+    });
+});
--- tests/integration/customer/signup.test.js
+++ tests/integration/customer/signup.test.js
@@ -1,18 +1,98 @@
+const { signupCustomer } = require('../../../controllers/customer/signup');
+const { pool } = require('../../../database/connection');
+
 let server;
 
-describe('Main suite description', () => {
-    beforeEach(() => {
-        server = require('../../../index');
+describe('customer/signup test cases', () => {
+    
+    describe('signupCustomer', () => {
+        beforeEach(async () => {
+            server = require('../../../index');
+        });
+
+        afterEach(async () => {
+            await server.close();
+        });
+
+        const req = {
+            body: {
+                "full_name": "Nalin Siriwardhana",
+                "nic": "883678783V",
+                "email": "nalin285@gmail.com",
+                "address": "12/7,first lane,Rajagiriya",
+                "latitude": "6.908951",
+                "longitude": "79.892441",
+                "gender": "MALE",
+                "dob": "1988-04-18",
+                "contact_no": "0775555555",
+                "password": "123456",
+                "confirm_password": "123456"
+            }
+        }
+        const res = {
+            render: jest.fn()
+        }
+        
+        it("400 Invalid sign up information", async () => {
+            req.body.confirm_password = "654321";
+
+            const expectedResult = {
+                    error_msg: "\"confirm_password\" must be [ref:password]",
+                    post_body: req.body
+            }
+            await signupCustomer(req, res);
+            expect(res.render).toHaveBeenCalledWith('customer/signup_error', expectedResult);
+
+        });
+
+        it("400 Email already registered", async () => {
+            req.body.email = "nimal1@gmail.com";
+            req.body.confirm_password = "123456";
+
+            const expectedResult = {
+                error_msg: "This email address has already been registered",
+                post_body: req.body
+            }
+            await signupCustomer(req, res);
+            expect(res.render).toHaveBeenCalledWith('customer/signup_error', expectedResult);
+
+        });
     });
 
-    afterEach(async () => {
-        await server.close();
+    describe('signupCustomer', () => {
+        const req = {
+            body: {
+                "full_name": "Nalin Siriwardhana",
+                "nic": "883678783V",
+                "email": "nalin285@gmail.com",
+                "address": "12/7,first lane,Rajagiriya",
+                "latitude": "6.908951",
+                "longitude": "79.892441",
+                "gender": "MALE",
+                "dob": "1988-04-18",
+                "contact_no": "0775555555",
+                "password": "123456",
+                "confirm_password": "123456"
+            }
+        }
+        const res = {
+            redirect: jest.fn()
+        }
+        beforeEach(async () => {
+            server = require('../../../index');
+            await pool.query("SET autocommit = OFF");
+            await pool.query("BEGIN");
+        });
 
-    });
+        afterEach(async () => {
+            await pool.query("ROLLBACK");
+            await pool.end();
+            await server.close();
+        });
 
-    describe('Suite description', () => {
-        it("Test description", async () => {
-
+        it("200 Customer sign up", async () => {
+            await signupCustomer(req, res);
+            expect(res.redirect).toHaveBeenCalledWith('/customer/login');
         });
     });
 });
