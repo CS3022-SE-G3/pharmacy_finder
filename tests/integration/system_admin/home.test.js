460714cf073da3d9141755d5396947db045058d8
Nagitha Abeywickrema <62171036+NagithaAbeywickrema@users.noreply.github.com>
2021-02-26 19:46:44 +0530

    ADD - system_admin/home test cases

--- tests/integration/system_admin/home.test.js
+++ tests/integration/system_admin/home.test.js
@@ -1,6 +1,8 @@
+const { viewHomePage } = require('../../../controllers/system_admin/home');
+
 let server;
 
-describe('Main suite description', () => {
+describe('/system_admin/home test cases', () => {
     beforeEach(() => {
         server = require('../../../index');
     });
@@ -10,9 +12,22 @@ describe('Main suite description', () => {
 
     });
 
-    describe('Suite description', () => {
-        it("Test description", async () => {
+    const res = {
+        status: jest.fn(() => res),
+        render: jest.fn(),
+        send: jest.fn(),
+        redirect: jest.fn()
+    };
 
+    const req = {};
+
+    describe('viewHomePage', () => {
+        it("it should return 200 and render system admin homepage", async () => {
+            viewHomePage(req,res);
+            expect(res.status).toHaveBeenLastCalledWith(200);
+            expect(res.render).toHaveBeenLastCalledWith('system_admin/home', {
+                pageTitle: 'Home'
+            });
         });
     });
 });
